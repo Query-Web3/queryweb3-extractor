f24ac560464b459db8fb0b7b6a1cb6aa57b2df74
Bison 'goldeagle' Fan <bison@bitseed.cn>
2025-05-05 12:26:39 +0800

    fix: entity name & prop

--- src/entities/DimStatCycle.ts
+++ src/entities/DimStatCycle.ts
@@ -1,15 +1,12 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
-@Entity()
+@Entity({ name: 'dim_stat_cycles' })
 export class DimStatCycle {
     @PrimaryGeneratedColumn()
     id: number;
 
     @Column({ length: 50 })
     name: string; // e.g. 'Daily', 'Weekly', 'Monthly', 'Quarterly', 'Yearly'
-
-    @Column({ length: 100 })
-    description: string;
 
     @Column({ type: 'int' })
     days: number; // Number of days in this cycle
